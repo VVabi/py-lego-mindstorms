25a5d79cf66efe2e206dd7159fa852e00a26ca70
vabi <vabian.lenhardt@gmail.com>
2022-07-03 19:14:58 +0200

    starting to add some structure

--- top_level.py
+++ top_level.py
@@ -8,15 +8,32 @@ import mindstorms
 import time
 from communication_helpers import *
 
+
+class Motors:
+    def __init__(self):
+        self.motors = dict()
+
+    
+    def handle_register_motor(self, config):
+        port = config["port"]
+        self.motors[port] = mindstorms.Motor(port)
+    
+    def handle_set_pwm(self, pwm_msg):
+        self.motors[pwm_msg["port"]].start(pwm_msg["pwm"])
+
+
 def top_level():
     hub_runtime.init(0)
+    handler_map = dict()
+    motors = Motors()
+    
+    handler_map["motor/register"] = motors.handle_register_motor
+    handler_map["motor/set_pwm"]  = motors.handle_set_pwm
+
     mshub   = mindstorms.MSHub()
-    motor_a = mindstorms.Motor('B')
-    motor_a.set_stop_action('hold')
-    motor_a.stop()
-    motor_a.run_to_position(0, 'clockwise', 50)
+    
     start_ts=time.time_ns()
-    for i in range(1000000):
+    while True:
         if not CommunicationHelper.is_connected():
             utime.sleep(0.5)
             continue
@@ -26,10 +43,13 @@ def top_level():
 
         gyro_data["pitch"]   = mshub.motion_sensor.get_pitch_angle()
         gyro_data["yaw"]     = mshub.motion_sensor.get_yaw_angle()
-        gyro_data["encoder"] = motor_a.get_position()
+        #gyro_data["encoder"] = motor_a.get_position()
         gyro_data["timestamp"] =(time.time_ns()-start_ts)/1000000
-        CommunicationHelper.write_dict_to_serial(gyro_data)
+        sensor_msg = {}
+        sensor_msg["topic"] = "sensor"
+        sensor_msg["payload"] = gyro_data
+        CommunicationHelper.write_dict_to_serial(sensor_msg)
         recv = CommunicationHelper.receive_json_from_serial()
-    
+        
         if recv is not None:
-            motor_a.start(recv["a"])
+            handler_map[recv["topic"]](recv["payload"])
